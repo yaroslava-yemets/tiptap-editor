0013425d34100a50cbb33c61226397d3c652bd9f
Yaroslava Yemets <77026107+yaroslava-yemets@users.noreply.github.com>
2022-10-28 14:20:56 +0200

    restart

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b612025919cb7899"; }
+/******/ 		__webpack_require__.h = function() { return "ca506db7e788c879"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
